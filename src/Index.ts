f2dfc07efb07239c2a15827313890bbae716ff51
Phillip Hoff <pmhoff@Phillips-MacBook-Pro.local>
2017-04-15 15:45:06 -0700

    Refactor intent registration.

--- src/Index.ts
+++ src/Index.ts
@@ -1,18 +1,20 @@
 import * as alexa from 'alexa-app';
 
+import IAlexaIntent from './intents/IAlexaIntent';
 import getCollectionDayIntent from './intents/GetCollectionDayIntent';
 import getCollectionTypeIntent from './intents/GetCollectionTypeIntent';
 
 const app = new alexa.app('spu');
 
-app.intent(
-    getCollectionDayIntent.name,
-    getCollectionDayIntent.schema,
-    getCollectionDayIntent.handler);
+function registerIntents(intents: IAlexaIntent[]): void {
+    intents.forEach(intent => {
+        app.intent(intent.name, intent.schema, intent.handler);
+    });
+}
 
-app.intent(
-    getCollectionTypeIntent.name,
-    getCollectionTypeIntent.schema,
-    getCollectionTypeIntent.handler);
+registerIntents([
+        getCollectionDayIntent,
+        getCollectionTypeIntent
+    ]);
 
 export = app;
